20dd928c9bb9b0dedee19e48a1b425b7679565b7
shouei661 <shouei661@gmail.com>
2025-08-03 15:18:26 +0800

    Update works-hotel.js

--- js/works-hotel.js
+++ js/works-hotel.js
@@ -45,13 +45,13 @@ const worksData = {
     },
     {
       img: "assets1/works-image/ALL-cafe-web.png",
-      title: "パーノワールカフェ（ＬＰ）",
+      title: "パーノワールカフェサイト",
       year: "2025",
       link: "cafe-web-detail.html"
     },
     {
       img: "assets1/works-image/All-hotel-web.png",
-      title: "シオラ海辺の旅館（ＬＰ）",
+      title: "シオラ海辺の旅館サイト",
       year: "2025",
       link: "hotel-web-detail.html"
     },
@@ -65,13 +65,13 @@ const worksData = {
   website: [
     {
       img: "assets1/works-image/ALL-cafe-web.png",
-      title: "パーノワールカフェ（ＬＰ）",
+      title: "パーノワールカフェサイト",
       year: "2025",
       link: "cafe-web-detail.html"
     },
     {
       img: "assets1/works-image/All-hotel-web.png",
-      title: "シオラ海辺の旅館（ＬＰ）",
+      title: "シオラ海辺の旅館サイト",
       year: "2025",
       link: "hotel-web-detail.html"
     },
